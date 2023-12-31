417aec520f94f88cb586d357c47727e78d64bf7c
N <contact@shot.codes>
2023-12-31 22:29:36 +0100

    added message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,4 @@
 <div class="flex h-full w-full items-center justify-center">
+    <h1>You want this dank domain? HMU @shot-codes - github</h1>
 	<img alt="deeznutz" class="h-[500px]" src="/deeznutz.jpg" />
 </div>
